8ad861e3ea8424343941ed8304836c149c7e1813
pratikktiwari <ipratiktiwari@gmail.com>
2021-02-15 23:22:26 +0530

    Fibonacci - Recursive

---
+++ Recursion-Backtracking-Day2/Fibonacci.java
@@ -0,0 +1,12 @@
+public class Fibonacci {
+    static int fibo(int n) {
+        if (n == 0 || n == 1)
+            return n;
+        return fibo(n - 1) + fibo(n - 2);
+    }
+
+    public static void main(String[] args) {
+        int res = fibo(5);
+        System.out.println("5th Fibo term: " + res);
+    }
+}
