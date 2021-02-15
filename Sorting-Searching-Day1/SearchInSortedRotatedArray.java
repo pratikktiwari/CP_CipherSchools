00acd084e3cc3875732db6784c08d0fe9c8f2c5b
pratikktiwari <ipratiktiwari@gmail.com>
2021-02-15 16:44:54 +0530

    Search In Sorted Rotated Array

---
+++ Sorting-Searching-Day1/SearchInSortedRotatedArray.java
@@ -0,0 +1,55 @@
+import java.util.Arrays;
+
+public class SearchInSortedRotatedArray {
+    static void binarySearch(int arr[], int start, int end, int x) {
+
+    }
+
+    // binarySearch(double[] a, int fromIndex, int toIndex, double key)
+    static int findPivot(int arr[], int start, int end) {
+        // boundary condition
+        if (start > end)
+            return -1;
+
+        int mid = start + (end - start) / 2;
+
+        if (start == mid)
+            return start;
+        // base condition
+        if (mid < end && arr[mid] > arr[mid + 1]) {
+            return mid;
+        }
+        if (start < mid && arr[mid - 1] > arr[mid]) {
+            return mid - 1;
+        }
+
+        if (arr[start] <= arr[mid]) {
+            // 1st segment is sorted
+            return findPivot(arr, mid + 1, end);
+        } else {
+            // second segment is sorted
+            return findPivot(arr, start, mid - 1);
+        }
+    }
+
+    static int searchInSortedRotatedArray(int arr[], int x) {
+        int i = 0, j = arr.length - 1;
+        int pivotElement = findPivot(arr, i, j);
+
+        int res1 = Arrays.binarySearch(arr, i, pivotElement, x);
+        int res2 = Arrays.binarySearch(arr, pivotElement + 1, j, x);
+
+        if (res1 < 0) {
+            return res2;
+        } else {
+            return res1;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 4, 6, 3, 7, 8 };
+        int res = searchInSortedRotatedArray(arr, 2);
+        System.out.println("Result index : " + res);
+    }
+}
