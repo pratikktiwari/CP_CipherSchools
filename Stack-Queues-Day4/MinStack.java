35c657621a701e8c13d2502cde8f86b1a7bd7923
pratikktiwari <ipratiktiwari@gmail.com>
2021-02-16 16:33:49 +0530

    Stack Operations

---
+++ Stack-Queues-Day4/MinStack.java
@@ -0,0 +1,42 @@
+import java.util.Stack;
+
+public class MinStack {
+    static int getMin;
+
+    static int getMinimum() {
+        return getMin;
+    }
+
+    static void push(Stack<Intger> s, int val) {
+        if (s.isEmpty()) {
+            s.push(val);
+            getMin = val;
+        } else {
+            int top = s.peek();
+            if (val >= getMin) {
+                s.push(val);
+            } else {
+                s.push(2 * val - getMin);
+                getMin = val;
+            }
+        }
+    }
+
+    static void pop(Stack<Integer> s) {
+        if (s.isEmpty()) {
+            return;
+        } else {
+            int y = s.peek();
+            if (y >= getMin) {
+                s.pop();
+            } else {
+                getMin = 2 * getMin - y;
+                s.pop();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
