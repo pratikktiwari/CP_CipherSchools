d40a7dae4aa1c723eee886e47da3900f861f52a5
pratikktiwari <ipratiktiwari@gmail.com>
2021-02-18 21:58:37 +0530

    Reorder List

--- LinkedLists-Day3/ReorderList.java
+++ LinkedLists-Day3/ReorderList.java
@@ -50,21 +50,52 @@ class LinkedList {
 
 public class ReorderList {
     static Node reorderList(Node head) {
+        // split list and merge them one by one
         if (head == null || head.next == null) {
             return head;
         }
         Node head1, head2;
         Node slowPtr = head, fastPtr = head;
+        Node pre = slowPtr;
         while (fastPtr != null && fastPtr.next != null) {
+            pre = slowPtr;
             slowPtr = slowPtr.next;
             fastPtr = fastPtr.next.next;
         }
+        pre.next = null;
+        head1 = head;
+        head2 = slowPtr;
+        Node dummy = new Node();
+        Node dummyRef = dummy;
+        while (head1 != null || head2 != null) {
+            if (head1 != null && head2 != null) {
+                dummy.next = head1;
+                head1 = head1.next;
+                dummy = dummy.next;
+
+                dummy.next = head2;
+                head2 = head2.next;
+                dummy = dummy.next;
+            } else if (head2 != null) {
+                dummy.next = head2;
+                head2 = head2.next;
+                dummy = dummy.next;
+            } else if (head1 != null) {
+                dummy.next = head1;
+                head1 = head1.next;
+                dummy = dummy.next;
+            }
+        }
+        dummy.next = null;
+        return dummyRef.next;
     }
 
     public static void main(String[] args) {
         LinkedList l = new LinkedList();
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i <= 10; i++) {
             l.add(i);
         }
+        l.head = reorderList(l.head);
+        l.printList();
     }
 }
