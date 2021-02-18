ddfe16b369a873e1652c06c8585810a272d0bcd1
pratikktiwari <ipratiktiwari@gmail.com>
2021-02-18 12:28:51 +0530

    Union Intersection of List

---
+++ LinkedLists-Day3/IntersectionOfTwoList.java
@@ -0,0 +1,99 @@
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int val) {
+        this.data = val;
+        this.next = null;
+    }
+
+    Node() {
+    }
+}
+
+class LinkedList {
+
+    Node head;
+
+    LinkedList add(int val) {
+        Node n1 = new Node(val);
+        n1.next = null;
+
+        if (this.head == null) {
+            this.head = n1;
+        } else {
+            Node ptr = this.head;
+            while (ptr.next != null) {
+                ptr = ptr.next;
+            }
+            ptr.next = n1;
+        }
+        return this;
+    }
+
+    void addAll(int arr[]) {
+        for (int x : arr) {
+            this.add(x);
+        }
+    }
+
+    void printList() {
+        Node ptr = this.head;
+        while (ptr != null) {
+            System.out.print(ptr.data + " ");
+            ptr = ptr.next;
+        }
+    }
+}
+
+public class IntersectionOfTwoList {
+    static void print(Node head) {
+        Node ptr = head;
+        System.out.println();
+        while (ptr != null) {
+            System.out.print(ptr.data + " ");
+            ptr = ptr.next;
+        }
+        System.out.println();
+    }
+
+    static Node findIntersection(Node head1, Node head2) {
+        if (head1 == null && head2 == null) {
+            return null;
+        }
+        if (head1 == null || head2 == null) {
+            return head1 == null ? head2 : head1;
+        }
+        Node dummy = new Node();
+        Node dummyRef = dummy;
+
+        while (head1 != null && head2 != null) {
+            if (head1.data < head2.data) {
+                head1 = head1.next;
+            } else if (head1.data > head2.data) {
+                head2 = head2.next;
+            } else {
+                dummy.next = head1;
+                dummy = dummy.next;
+                head1 = head1.next;
+                head2 = head2.next;
+            }
+        }
+        dummy.next = null;
+        return dummyRef.next;
+
+    }
+
+    public static void main(String[] args) {
+        LinkedList l1 = new LinkedList();
+        LinkedList l2 = new LinkedList();
+        for (int i = 1; i < 5; i++) {
+            l1.add(i + 4);
+            l2.add(i * 2);
+        }
+        Node newNode = findIntersection(l1.head, l2.head);
+        print(newNode);
+    }
+}
