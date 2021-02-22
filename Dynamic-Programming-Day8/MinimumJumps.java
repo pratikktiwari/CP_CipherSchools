acd40c84fa5e3a63ecd25bad7305de68eaca06d3
pratikktiwari <ipratiktiwari@gmail.com>
2021-02-22 16:15:40 +0530

    DP

---
+++ Dynamic-Programming-Day8/MinimumJumps.java
@@ -0,0 +1,15 @@
+public class MinimumJumps {
+    int getMinJumps(int N) {
+        if (n == 0) {
+            return 1;
+        }
+        int dp[] = new int[N + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
